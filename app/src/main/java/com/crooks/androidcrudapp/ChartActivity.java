31baa3d7646bb827cee45fe2a9445224a4c09d05
John Crooks <johnmcrooks@gmail.com>
2016-10-14 16:58:36 -0400

    Friday 5 oclock Commit Time. Will reconvene later this weekend.
    TODO:
    -Expand utility ( Export data[cloud/local], Import Data, add Odometer watch and MPG tracking, more charts[hopefully])
    -Beautify

--- app/src/main/java/com/crooks/androidcrudapp/ChartActivity.java
+++ app/src/main/java/com/crooks/androidcrudapp/ChartActivity.java
@@ -1,5 +1,6 @@
 package com.crooks.androidcrudapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,8 +31,9 @@ public class ChartActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
         Button buttonBack = (Button) findViewById(R.id.buttonReturnToMain);
-
         buttonBack.setOnClickListener(this);
+
+
 
         createTotalCostTable();
     }
@@ -42,6 +44,27 @@ public class ChartActivity extends AppCompatActivity implements View.OnClickList
         chart.setNoDataTextDescription("No data to analyze yet!");
         chart.setNoDataText("No data to analyze yet");
 
+
+        //This swipe code was found on StackOverflow (more details in the OnSwipeTouchListener Class) and re-purposed
+        // for my particular use case. ITS SO COOL!
+        chart.setOnTouchListener(new OnSwipeTouchListener(ChartActivity.this) {
+            public void onSwipeTop() {
+                Toast.makeText(ChartActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+            public void onSwipeRight() {
+                Toast.makeText(ChartActivity.this, "right", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ChartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+            public void onSwipeLeft() {
+                Toast.makeText(ChartActivity.this, "left", Toast.LENGTH_SHORT).show();
+
+            }
+            public void onSwipeBottom() {
+                Toast.makeText(ChartActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
         ArrayList<BarEntry> entries = new ArrayList<>();
         List<FillUp> fillUpList = new TableControllerFillUp(this).read();
 
@@ -57,7 +80,6 @@ public class ChartActivity extends AppCompatActivity implements View.OnClickList
         BarData lineData = new BarData(dataset);
         lineData.setValueTextSize(12);
         chart.setData(lineData);
-
 
         chart.invalidate();
     }
