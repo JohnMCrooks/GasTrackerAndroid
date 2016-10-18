0e2b6f2cfaf213afbfecdfba463d450a09d12e59
John Crooks <johnmcrooks@gmail.com>
2016-10-18 15:10:20 -0400

    commit before updating Android Studio and installing latest SDK. TODO: -Expand utility ( Export data[cloud/local], Import Data, add Odometer watch and MPG tracking, more charts[hopefully]) -Beautify

--- app/src/test/java/com/crooks/androidcrudapp/SqlTesting.java
+++ app/src/test/java/com/crooks/androidcrudapp/SqlTesting.java
@@ -1,20 +1,39 @@
 package com.crooks.androidcrudapp;
 
 import android.content.Context;
-import android.test.AndroidTestCase;
-import android.test.mock.MockContext;
+import android.test.RenamingDelegatingContext;
+import android.test.ActivityInstrumentationTestCase2;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.model.TestClass;
 
 /**
  * Created by johncrooks on 10/18/16.
  */
+@RunWith(AndroidJUnit4.class)
+public class SqlTesting extends ActivityInstrumentationTestCase2<TableControllerFillUp>{
+    private DbHandler mDB;
 
-public class SqlTesting extends AndroidTestCase {
+    public SqlTesting(Class<TableControllerFillUp> activityClass) {
+        super(activityClass);
+    }
 
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+        mDB = new DbHandler(context);
+    }
 
-    Context mContext = new MockContext();
+    @After
+    public void tearDown() throws Exception {
+        mDB.close();
+        super.tearDown();
+    }
 
     @Test
     public void testInitialAlwaysPasses(){
@@ -24,7 +43,6 @@ public class SqlTesting extends AndroidTestCase {
     @Test
     public void testdbDrop (){
         // TODO: 10/18/16 Write some unit tests to help determine SQL issue editing/updating db.
-
         assertTrue(mContext.deleteDatabase(DbHandler.DATABASE_NAME));
 
 //        boolean createSuccessful = new TableControllerFillUp(context).createRecord(newTank);
